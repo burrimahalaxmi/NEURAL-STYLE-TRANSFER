5bea5cd85bfdf074bb0c0f81dc2749d14927aad2
burrimahalaxmi <burri.mahalaxmi@gmail.com>
2026-05-22 14:35:24 +0530

    Add files via upload

---
+++ index3.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+import os
+
+# List files in the current directory
+print('Files in current directory:')
+for dirname, _, filenames in os.walk('.'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+# Helper function to check if a file exists
+def check_file_exists(filepath):
+    if not os.path.exists(filepath):
+        print(f"Error: File not found at '{filepath}'")
+        return False
+    return True
+
+# 1. Load and Preprocess Images
+def load_img(path_to_img):
+    if not check_file_exists(path_to_img):
+        return None
+
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor) > 3:
+        tensor = tensor[0]
+    return PIL.Image.fromarray(tensor)
+
+# 2. Load Pre-trained VGG19 (Feature Extractor)
+vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+vgg.trainable = False
+
+# Content layer where we will pull our feature maps
+content_layers = ['block5_conv2']
+
+# Style layers of interest
+style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+
+def vgg_layers(layer_names):
+    outputs = [vgg.get_layer(name).output for name in layer_names]
+    return tf.keras.Model([vgg.input], outputs)
+
+# 3. Define Style and Content Models
+class StyleContentModel(tf.keras.models.Model):
+    def __init__(self, style_layers, content_layers):
+        super(StyleContentModel, self).__init__()
+        self.vgg = vgg_layers(style_layers + content_layers)
+        self.style_layers = style_layers
+        self.content_layers = content_layers
+        self.num_style_layers = len(style_layers)
+
+    def call(self, inputs):
+        inputs = inputs * 255.0
+        preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+        outputs = self.vgg(preprocessed_input)
+        style_outputs, content_outputs = (outputs[:self.num_style_layers],
+                                          outputs[self.num_style_layers:])
+
+        style_outputs = [self.gram_matrix(style_output) for style_output in style_outputs]
+        content_dict = {name: value for name, value in zip(self.content_layers, content_outputs)}
+        style_dict = {name: value for name, value in zip(self.style_layers, style_outputs)}
+        return {'content': content_dict, 'style': style_dict}
+
+    def gram_matrix(self, input_tensor):
+        result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+        input_shape = tf.shape(input_tensor)
+        num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+        return result / num_locations
+
+# 4. Initialization & Optimization
+content_image = load_img('path_to_your_photo.jpg')
+style_image = load_img('path_to_artistic_style.jpg')
+
+if content_image is None or style_image is None:
+    print("Image loading failed. Please ensure both content and style images exist at the specified paths.")
+else:
+    extractor = StyleContentModel(style_layers, content_layers)
+    style_targets = extractor(style_image)['style']
+    content_targets = extractor(content_image)['content']
+
+    image = tf.Variable(content_image) # Start with content image
+    opt = tf.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+    @tf.function()
+    def train_step(image):
+        with tf.GradientTape() as tape:
+            outputs = extractor(image)
+            # Calculate Loss
+            s_loss = tf.add_n([tf.reduce_mean((outputs['style'][name] - style_targets[name])**2) for name in outputs['style'].keys()])
+            c_loss = tf.add_n([tf.reduce_mean((outputs['content'][name] - content_targets[name])**2) for name in outputs['content'].keys()])
+            total_loss = s_loss * 1e-2 + c_loss * 1e4 # Weights for style vs content
+
+        grad = tape.gradient(total_loss, image)
+        opt.apply_gradients(zip([grad], [image])) # Pass gradients and variables to apply_gradients
+        image.assign(tf.clip_by_value(image, 0.0, 1.0))
+
+    # Run optimization for 1000 steps
+    for i in range(1000):
+        train_step(image)
+
+    # Save result
+    result = tensor_to_image(image)
+    result.save('styled_image.jpg')
